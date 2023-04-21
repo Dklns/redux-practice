730b24617841c0ae30ba3db09ae7b8fd7a509def
Dklns <d.klns.245@gmail.com>
2023-04-21 22:52:14 +0800

    update: React Redux Full Course for Beginners | Redux Toolkit Complete Tutorial ----Async Logic & Thunks

--- redux-toolkit-complete-tutorial/src/features/posts/AddPostForm.jsx
+++ redux-toolkit-complete-tutorial/src/features/posts/AddPostForm.jsx
@@ -1,30 +1,39 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { postAdded } from "./postsSlice";
+
+import { addNewPost } from "./postsSlice";
 import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
     const [title, setTitle] = useState('');
     const [content, setContent] = useState('');
     const [userId, setUserId] = useState('');
+    const [addRequestStatus, setAddRequestStatus] = useState('idle');
+
     const dispatch = useDispatch();
+
     const users = useSelector(selectAllUsers);
-    const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
+
+    const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
 
     const onTitleChanged = e => setTitle(e.target.value);
     const onContentChanged = e => setContent(e.target.value);
     const onAuthorChanged = e => setUserId(e.target.value);
 
     const onSavePostClicked = () => {
-        if(title && content) {
-            dispatch(postAdded({
-                title,
-                content,
-                userId
-            }));
+        if(canSave) {
+            try {
+                setAddRequestStatus('pending');
+                dispatch(addNewPost({title, body: content, userId})).unwrap()
 
-            setTitle('');
-            setContent('');
+                setTitle('');
+                setContent('');
+                setAddRequestStatus('idle');
+            } catch (error) {
+                console.log(error);
+            } finally {
+                setAddRequestStatus('idle');
+            }
         }
     }
 
--- redux-toolkit-complete-tutorial/src/features/posts/PostsList.jsx
+++ redux-toolkit-complete-tutorial/src/features/posts/PostsList.jsx
@@ -1,29 +1,38 @@
-import { useSelector } from "react-redux";
-import { selectAllPost } from "./postsSlice";
-import PostAuthor from "./PostAuthor";
-import TimeAgo from "./TimeAgo";
-import ReactionButtons from "./ReactionButtons";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectAllPost, getPostError, getPostStatus,fetchPosts } from "./postsSlice";
+import PostsExcerpt from "./PostsExcerpt";
+
 
 const PostsList = () => {
-    const posts = useSelector(selectAllPost);
-    const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date))
+    const dispatch = useDispatch();
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.content.substring(0, 100)}</p>
-            <p className="postCredit">
-                <PostAuthor userId={post.userId}/>
-                <TimeAgo timestamp={post.date}/>
-            </p>
-            <ReactionButtons post={post} />
-        </article>
-    ))
+    const posts = useSelector(selectAllPost);
+    const postStatus = useSelector(getPostStatus);
+    const postError = useSelector(getPostError);
+
+    useEffect(() => {
+        if(postStatus === 'idle') {
+            dispatch(fetchPosts())
+        }
+    }, [postStatus, dispatch]);
+
+    
+
+    let content;
+    if(postStatus === 'loading') {
+        content = <p>Loading....</p>
+    } else if (postStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a,b) => b.date.localeCompare(a.date));
+        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post}/>)
+    } else if(postStatus === 'failed') {
+        content = <p>{postError}</p>
+    }
 
     return (
         <section>
             <h2>Posts</h2>
-            {renderedPosts}
+            {content}
         </section>
     )
 }
